ce53f59be7eff021fbd24d61f6f06e251bb5a069
vexleet <abda70226@gmail.com>
2022-03-28 15:08:58 +0200

    add about section

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,10 @@
 const name = document.querySelector('.header .name');
 const subtitle = document.querySelector('.header .subtitle');
 const scrollDown = document.querySelector('.header .scroll-down');
-
+const playVideo = document.getElementById('play-video');
+const modal = document.getElementById("myModal");
+const closeModal = document.querySelector('#myModal .close');
+const documentaryVideo = document.querySelector('#myModal #documentary')
 anime({
   targets: name,
   fontSize: 120,
@@ -23,4 +26,24 @@ anime({
   direction: 'alternate',
   loop: true,
   easing: 'spring(1, 80, 10, 0)'
+})
+
+new Pageable("#fullpage", {
+  animation: 400,
+  delay: 200,
+  events: {
+    wheel: true,
+    mouse: false,
+  }
+});
+
+playVideo.addEventListener('click', function () {
+  modal.style.display = "block";
+  documentaryVideo.play();
+});
+
+closeModal.addEventListener('click', function () {
+  modal.style.display = "none";
+  documentaryVideo.pause();
+  documentaryVideo.currentTime = 0;
 })
